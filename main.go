b810c27acc525eb44cac9877544f4ad82a75f186
Bill Glover <bill@billglover.co.uk>
2017-03-12 13:02:32 +0000

    Read data file line by line

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "log"
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	log.Println("Hello, World!")
+	f, err := os.Open("data/test_data.txt")
+	defer f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := bufio.NewReader(f)
+	for {
+		name, err := r.ReadString('\n')
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print(name)
+	}
 }
