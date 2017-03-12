f490bbd5acda75008131a4fdc69976d1d8f14afc
Bill Glover <bill@billglover.co.uk>
2017-03-12 15:21:23 +0000

    Attempt 1

--- main.go
+++ main.go
@@ -2,27 +2,69 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"io"
 	"log"
+	"math/rand"
 	"os"
+	"sort"
+	"strings"
+	"time"
 )
 
+var (
+	count = flag.Int("n", 1, "the number of names to pick from the list")
+)
+
+type person struct {
+	Name  string
+	Score int
+}
+
+type byScore []person
+
+func (a byScore) Len() int           { return len(a) }
+func (a byScore) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byScore) Less(i, j int) bool { return a[i].Score < a[j].Score }
+
 func main() {
+	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
+
+	var list []person
+
+	// open the list of names
+	// TODO: should be able to accept STDIN as well
 	f, err := os.Open("data/test_data.txt")
 	defer f.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// read the names into memory and assign each a random score
 	r := bufio.NewReader(f)
 	for {
 		name, err := r.ReadString('\n')
 		if err == io.EOF {
-			return
+			break
 		}
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Print(name)
+
+		p := person{
+			Name:  strings.TrimRight(name, "\n"),
+			Score: rand.Int(),
+		}
+		list = append(list, p)
+
+	}
+
+	// sort the list in ascending order
+	sort.Sort(byScore(list))
+
+	// return the number of names requested
+	for c := 0; c < *count; c++ {
+		log.Printf("%s\n", list[c].Name)
 	}
 }
